e6d2e414f28c34f0afb3fcfd91adb23eb3b42ee7
takata-kento <takata_kento@ims-sol.co.jp>
2024-08-18 17:10:50 +0900

    リファクタリングとエラーハンドリングの追加

--- lambda/srcLambdaFunc/lambda_function.py
+++ lambda/srcLambdaFunc/lambda_function.py
@@ -1,24 +1,41 @@
 from langchain_community.llms.bedrock import Bedrock
 import boto3
+import json
+
+bedrock = boto3.client(
+    service_name='bedrock-runtime',
+    region_name='us-east-1',
+)
+
+llm = Bedrock(
+    model_id='anthropic.claude-3-5-sonnet-20240620-v1:0',
+    client = bedrock,
+    model_kwargs={
+        "max_tokens_to_sample": 4096,
+        "temperature": 0,
+        "stop_sequences": []
+    },
+    region_name='us-east-1',
+)
 
 def lambda_handler(event, context):
-    base64 = event['base64']
+    if 'base64' not in event:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('Error: "base64" key not found in the event')
+        }
+
+    base64_data = event['base64']
     question = 'Base64でエンコードされた画像を添付するのでそこに記載されている文字を抽出してください。'
-    bedrock = boto3.client(
-        service_name='bedrock-runtime',
-        region_name='us-east-1',
-    )
 
-    llm = Bedrock(
-        model_id='anthropic.claude-3-5-sonnet-20240620-v1:0',
-        client = bedrock,
-        model_kwargs={
-            "max_tokens_to_sample": 4096,
-            "temperature": 0,
-            "stop_sequences": []
-        },
-        region_name='us-east-1',
-    )
-
-    response = llm.predict(question + '\n' + base64)
-    return response
+    try:
+        response = llm.predict(question + '\n' + base64_data)
+        return {
+            'statusCode': 200,
+            'body': response
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f'Error occurred: {str(e)}')
+        }
