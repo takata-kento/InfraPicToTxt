341c3757cfefac12b6b52d9b28aeffd29ea1a1c4
takata-kento <takata_kento@ims-sol.co.jp>
2024-09-03 01:15:15 +0900

    index.tsの追加

--- index.ts
+++ index.ts
@@ -1,9 +1,44 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as IamRole from "./iam/iam";
+import * as Lambda from "./lambda/lambda";
+import * as Apigateway from "./apigateway/apigateway";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const role = new IamRole.IAMRole(
+    "roleLambdaPicToTxt",
+    "policyLambdaPicToTxt",
+    "./iam/policies/lambda_app_policy.json",
+    "./iam/policies/lambda_app_assume_policy.json",
+    {
+         "App": "PicToTxt"
+    }
+);
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+export const roleResource = role.createIAMRole();
+
+const lambda = new Lambda.ResourceLambda(
+    "roleLambdaPicToTxt",
+    "FunctionLambdaPicToTxt",
+    "./lambda/srcLambdaFunc/lambda_function.py",
+    {
+        "App": "PicToTxt"
+    }
+);
+
+export const lambdaResource = lambda.create(undefined, "./lambda/srcLambdaFunc/packages.zip");
+
+// const apigateway = new Apigateway.APIGateway(
+//     "ApiPicToTxt",
+//     [
+//         {
+//             method: "POST",
+//             apiPath: "/picToTxt",
+//             lambda_handler: lambdaResource,
+//             lambda_auth_handler: undefined
+//         }
+//     ],
+//     "dev",
+//     {
+//         "App": "PicToTxt"
+//     }
+// );
+
+// export const apiGatewayResource = apigateway.createAPIGateway();
